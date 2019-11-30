4da3f8a8e8a7937ecb4f73463b7e334ea75ea316
Timur Saifullin <timursayfullin@gmail.com>
2019-11-30 22:54:31 +0200

    first commit

---
+++ src/test/java/services/dashboardapi/DashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package services.dashboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
